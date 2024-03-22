058fc89944885111b2190e4b32b71fec776f88eb
Dan Nixon <dan@dan-nixon.com>
2024-03-22 20:36:41 +0000

    Use Rust toolchain from NixOS stable

--- flake.nix
+++ flake.nix
@@ -1,42 +1,35 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    fenix,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = (import nixpkgs) {
           inherit system;
         };
-
-        toolchain = fenix.packages.${system}.toolchainOf {
-          channel = "1.74";
-          date = "2023-12-07";
-          sha256 = "PjvuouwTsYfNKW5Vi5Ye7y+lL7SsWGBxCtBOOm2z14c=";
-        };
       in {
         devShell = pkgs.mkShell {
           packages = with pkgs; [
             # Rust toolchain
-            toolchain.toolchain
+            cargo
+            rustc
 
-            # Formatting tools
+            # Code analysis tools
+            rust-analyzer
+            clippy
+
+            # Code formatting tools
             treefmt
             alejandra
             mdl
+            rustfmt
           ];
         };
       }
